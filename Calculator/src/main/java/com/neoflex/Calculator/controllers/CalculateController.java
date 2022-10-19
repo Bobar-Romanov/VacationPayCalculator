ff093435d1c42a1590da9558dd08472d3d0fed10
Roman <roma6404999@yandex.ru>
2022-10-19 22:58:57 +0400

    vacation pay

--- Calculator/src/main/java/com/neoflex/Calculator/controllers/CalculateController.java
+++ Calculator/src/main/java/com/neoflex/Calculator/controllers/CalculateController.java
@@ -2,17 +2,16 @@ package com.neoflex.Calculator.controllers;
 
 
 
-import com.neoflex.Calculator.models.VacantionPay;
+
 import com.neoflex.Calculator.services.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+
 
 @RestController
     public class CalculateController {
--- Calculator/src/main/java/com/neoflex/Calculator/services/CalculateService.java
+++ Calculator/src/main/java/com/neoflex/Calculator/services/CalculateService.java
@@ -4,13 +4,12 @@ package com.neoflex.Calculator.services;
 import com.neoflex.Calculator.clients.DayOffFeignClient;
 import com.neoflex.Calculator.models.VacantionPay;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.zip.DataFormatException;
+
 
 @Service
 public class CalculateService {
@@ -22,43 +21,40 @@ public class CalculateService {
         this.dayOffFeignClient = dayOffFeignClient;
     }
 
-    @Autowired
-    private Environment environment;
 
     public VacantionPay calculateVacantionPay(String strSalary, String strDays, String strStartDate){
-            double salary = 0;
-            int days = 0;
             try {
+                double salary = 0;
+                int days = 0;
                 salary = Double.parseDouble(strSalary);
                 days = Integer.parseInt(strDays);
+                int count = days;
+                if(days <= 0 || salary <= 0){
+                    return new VacantionPay();
+                }
+                if(strStartDate == null){
+                    VacantionPay vacantionPay = new VacantionPay(salary, days, (salary/29.3)*count);
+                    return vacantionPay;
+                }
+                LocalDate startDate = null;
+
+                DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                startDate = LocalDate.parse(strStartDate, dateformatter);
+
+                LocalDate curDate = startDate;
+                for(int i = 0; i < days; i++){
+                    if(dayOffFeignClient.getDayOff(curDate).equals("1") && !curDate.getDayOfWeek().toString().equals("SATURDAY")
+                            && !curDate.getDayOfWeek().toString().equals("SUNDAY") ) {
+                        count--;
+                    }
+                    curDate = curDate.plusDays(1);
+                }
+                return new VacantionPay(salary, days, startDate,(salary/29.3)*count);
             }catch (NumberFormatException e){
                return new VacantionPay();
             }
-            int count = days;
-            if(days <= 0 || salary <= 0){
+            catch (DateTimeParseException e){
                 return new VacantionPay();
             }
-            if(strStartDate == null){
-                VacantionPay vacantionPay = new VacantionPay(salary, days, (salary/29.3)*count);
-                return vacantionPay;
-            }
-            LocalDate startDate = null;
-            try{
-                DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                startDate = LocalDate.parse(strStartDate, dateformatter);
-            }catch (DateTimeParseException e){
-                return new VacantionPay();
-            }
-            LocalDate curDate = startDate;
-            for(int i = 0; i < days; i++){
-            if(dayOffFeignClient.getDayOff(curDate).equals("1") && !curDate.getDayOfWeek().toString().equals("SATURDAY")
-                    && !curDate.getDayOfWeek().toString().equals("SUNDAY") ) {
-                        count--;
-                }
-                curDate = curDate.plusDays(1);
-            }
-            System.out.println(salary * count);
-
-            return new VacantionPay(salary, days, startDate,(salary/29.3)*count);
         }
 }
